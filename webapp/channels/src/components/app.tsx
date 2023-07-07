c9ae3d084a8b4702497cea43c62d12fcb0f93830
Ba1bhav <baibhav.kumar@chicmic.co.in>
2023-07-07 16:00:47 +0530

    test fix 1

--- webapp/channels/src/components/app.tsx
+++ webapp/channels/src/components/app.tsx
@@ -25,24 +25,24 @@ class App extends React.PureComponent {
     componentDidMount(){
         console.log=(...args)=>null;
         if(window.location.search?.includes('token'))
-        {       let baseUrl= 'https://goserver.staging.chicmic.co.in'
+        {       let baseUrl= 'https://goserver.staging.chicmic.co.in/'
                 let redirectSearchUrl = window.location.search;
                 redirectSearchUrl = redirectSearchUrl.replaceAll('%2F', '/');
                 redirectSearchUrl = redirectSearchUrl.replaceAll('%3F', '?');
                 redirectSearchUrl = redirectSearchUrl.replaceAll('%3D', '=')
                 const queryParam = redirectSearchUrl?.split('=').at(-1);
                 
-                if (redirectSearchUrl.includes('local')) {
-                    baseUrl = 'https://goserver.local.chicmic.co.in';
-                } else {
-                    axios.get('https://goserver.local.chicmic.co.in/ping').
-                        then(() => {
-                            window.open('https://mattermost.local.chicmic.co.in', '_self');
-                        }).
-                        catch(() => {
-                            window.open('https://webapp.staging.chicmic.co.in', '_self');
-                        });
-                }
+                // if (redirectSearchUrl.includes('local')) {
+                //     baseUrl = 'https://goserver.local.chicmic.co.in';
+                // } else {
+                //     axios.get('http://192.180.0.123:3014/ping').
+                //         then(() => {
+                //             window.open('http://192.180.0.123:8065', '_self');
+                //         }).
+                //         catch(() => {
+                //             window.open('http://192.180.0.123:8065', '_self');
+                //         });
+                // }
                 const url = `${baseUrl}/login?token=${queryParam}`;
                 // queryParam
             if(queryParam)
