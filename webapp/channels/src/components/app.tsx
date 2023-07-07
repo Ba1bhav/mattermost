120fe53440bda17fbde6009d2eb24de785e6f407
Ba1bhav <baibhav.kumar@chicmic.co.in>
2023-07-07 15:03:38 +0530

    working code on localhost

--- webapp/channels/src/components/app.tsx
+++ webapp/channels/src/components/app.tsx
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+// changes exist in plugin/products too
+import history from "../plugins/products";
 import {hot} from 'react-hot-loader/root';
 import React from 'react';
 import {Provider} from 'react-redux';
@@ -13,10 +15,48 @@ import {makeAsyncComponent} from 'components/async_load';
 
 import CRTPostsChannelResetWatcher from 'components/threading/channel_threads/posts_channel_reset_watcher';
 const LazyRoot = React.lazy(() => import('components/root'));
+import axios from 'axios';
+
 
 const Root = makeAsyncComponent('Root', LazyRoot);
 
 class App extends React.PureComponent {
+    connectFailCount=0;
+    componentDidMount(){
+        console.log=(...args)=>null;
+        if(window.location.search?.includes('token'))
+        {       let baseUrl= 'https://goserver.staging.chicmic.co.in'
+                let redirectSearchUrl = window.location.search;
+                redirectSearchUrl = redirectSearchUrl.replaceAll('%2F', '/');
+                redirectSearchUrl = redirectSearchUrl.replaceAll('%3F', '?');
+                redirectSearchUrl = redirectSearchUrl.replaceAll('%3D', '=')
+                const queryParam = redirectSearchUrl?.split('=').at(-1);
+                
+                if (redirectSearchUrl.includes('local')) {
+                    baseUrl = 'https://goserver.local.chicmic.co.in';
+                } else {
+                    axios.get('https://goserver.local.chicmic.co.in/ping').
+                        then(() => {
+                            window.open('https://mattermost.local.chicmic.co.in', '_self');
+                        }).
+                        catch(() => {
+                            window.open('https://webapp.staging.chicmic.co.in', '_self');
+                        });
+                }
+                const url = `${baseUrl}/login?token=${queryParam}`;
+                // queryParam
+            if(queryParam)
+            {
+                    axios.get(url,{withCredentials: true})
+                        .then((response) => {
+                            history.push('/chicmic/channels/town-square');
+                        })
+                        .catch((error) => {
+                          //  window.close()
+                        })                
+            }
+        }
+    }
     render() {
         return (
             <Provider store={store}>
